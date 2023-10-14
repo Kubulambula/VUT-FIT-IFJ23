25b3370e37cfcc0c567273c82cdab224f9d2f8c3
Jakub Slanina <xslani12@vutbr.cz>
2023-10-14 16:27:13 +0200

    added additional functions

--- src/lexer.c
+++ src/lexer.c
@@ -1,22 +1,91 @@
-#include <stdio.h>
 #include "error.h"
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <stdio.h>
 
-// Prvek jednoducheho seznamu
-typedef struct TonekNode {
-    // Typ tokenu. (Budem resit cislem teda?)
-    int type;
-    // Ukazatel na následující prvek seznamu.
-    struct TonekNode* next;
-} TonekNode;
+#include "buffer_string.h"
+#include "lexer.h"
 
-// Jednoduchy seznam
-typedef struct TokenList{
-    // Ukazatel na "hlavicku" seznamu
-    TonekNode* head;
-} TokenList;
+#define Identifier 1
+#define Operator 2
+#define Constant 3
+#define Keyword 4
+#define Special_Char 5
+
+
+FILE* g_file; 
+
+
+
+
+
+void initLexer(char* filename){
+    //printf("%s",filename);
+    g_file = fopen(filename, "r");
+    if (g_file == NULL) {
+        perror("Error opening file for reading");
+        exit(1); 
+    }
+};
+
+
+//
+char get_next_char(){
+    char Nextchar = getc(g_file);
+    //printf("%C",Nextchar);
+    return Nextchar;
+};
+
+// Handle whitespaces
+void skip_white_space(char* whiteSpace){
+    do{
+        *whiteSpace = get_next_char();
+    } while(*whiteSpace == ' ');
+};
+
+// Handle comments mutli line
+void skip_comments_ML(){
+    char Char;
+    char prevChar;
+    while(1){
+        prevChar = Char;
+        Char = get_next_char();
+        if(prevChar == '*' && Char == "/"){return;}
+    }
+};
+
+// Handle comments single line
+void skip_comments_SL(){
+    char Char = get_next_char();
+    while(Char != "\n"){
+        Char = get_next_char();
+    }
+};
 
 // Funkce najde a vytvori token
-void get_next_token();
+void get_next_token(/*BufferString* id, Token* token*/){
+    char nextChar = get_next_char();
+    if(nextChar == EOF){
+        exit;
+    }
 
-// Svaze tokeny na stejnem radku a vrati je
-void get_next_line();
+    if(nextChar == ' '){
+        skip_white_space(nextChar);
+    }
+
+    if(nextChar == '/'){
+        nextChar = get_next_char();
+        if(nextChar == '*'){skip_comments_ML();}
+        if(nextChar == '/'){skip_comments_SL();}
+    }
+
+
+
+};
+
+int main(int argc, char* argv[]) {
+	initLexer("test");
+    get_next_token();
+	return OK;
+}
