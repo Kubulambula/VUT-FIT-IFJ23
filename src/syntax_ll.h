9fd299bf9c9d9ded596ce7b17f9f7082463d105b
Jakub Janšta <kubulambula@gmail.com>
2023-11-29 18:50:57 +0100

    idk

--- src/syntax_ll.h
+++ src/syntax_ll.h
@@ -16,42 +16,48 @@
 		return (CURRENT_TOKEN == TOKEN_ERR_INTERNAL ? ERR_INTERNAL : ERR_LEXICAL); \
 
 
+
 Error ll_program(BufferString* buffer_string, ASTNode** tree);
 
 Error ll_func_definition(BufferString* buffer_string, ASTNode** tree);
 
-Error ll_func_definition_head(BufferString* buffer_string, char** func_name);
+Error ll_func_definition_head(BufferString* buffer_string, ASTNode** func_head);
 
-Error ll_func_definition_head_args(BufferString* buffer_string, SymTable* table, Symbol* func_symbol);
+Error ll_func_definition_head_args(BufferString* buffer_string, FuncDefArg** func_args);
 
-Error ll_func_definition_head_arg(BufferString* buffer_string, SymTable* table, Symbol* func_symbol);
+Error ll_func_definition_head_arg(BufferString* buffer_string, FuncDefArg** func_arg);
 
 Error ll_func_definition_body(BufferString* buffer_string, ASTNode** tree);
 
-Error ll_statements(BufferString* buffer_string, SymTable* table, ASTNode** tree, char* scope);
+Error ll_statements(BufferString* buffer_string, ASTNode** tree);
 
-Error ll_let_var_declaration(BufferString* buffer_string, SymTable* table, ASTNode** tree, char* scope, SymbolType symbol_type, ASTNodeType node_type);
+Error ll_func_call(BufferString* buffer_string, ASTNode** tree, char* func_name);
+Error ll_func_call_args(BufferString* buffer_string, ASTNode** tree);
 
-Error ll_var_declaration(BufferString* buffer_string, SymTable* table, ASTNode** tree, char* scope);
+Error ll_func_call_arg(BufferString* buffer_string, ASTNode** tree);
 
-Error ll_let_declaration(BufferString* buffer_string, SymTable* table, ASTNode** tree, char* scope);
+Error ll_func_call_arg_with_name(BufferString* buffer_string, ASTNode* tree);
 
-Error ll_assign(BufferString* buffer_string, SymTable* table, ASTNode** tree, char* var_name);
+Error ll_func_call_arg_without_name(BufferString* buffer_string, ASTNode* tree);
 
-Error ll_func_call(BufferString* buffer_string, SymTable* table, ASTNode** tree, char* func_name);
+Error ll_assign(BufferString* buffer_string, ASTNode** tree, char* var_name);
 
-Error ll_func_call_args(BufferString* buffer_string, SymTable* table, ASTNode* tree);
+Error ll_var_declaration(BufferString* buffer_string, ASTNode* tree);
 
-Error ll_func_call_arg(BufferString* buffer_string, SymTable* table, ASTNode* tree);
+Error ll_var_declaration(BufferString* buffer_string, ASTNode** tree);
 
-Error ll_func_call_arg_with_name(BufferString* buffer_string, SymTable* table, ASTNode* tree);
+Error ll_let_declaration(BufferString* buffer_string, ASTNode** tree);
 
-Error ll_func_call_arg_without_name(BufferString* buffer_string, SymTable* table, ASTNode* tree);
+Error ll_while(BufferString* buffer_string, ASTNode** tree);
 
-Error ll_while(BufferString* buffer_string, void* tree);
+Error ll_return(BufferString* buffer_string, ASTNode** tree);
 
-Error ll_return(BufferString* buffer_string, void* tree);
+Error ll_if(BufferString* buffer_string, ASTNode** tree);
 
-Error ll_if(BufferString* buffer_string, void* tree);
+Error generate_if_let_declaration_override_statement(ASTNode** tree, char* id);
+
+Error ll_if_head(BufferString* buffer_string, ASTNode* tree);
+
+Error ll_else(BufferString* buffer_string, ASTNode** tree);
 
 #endif
