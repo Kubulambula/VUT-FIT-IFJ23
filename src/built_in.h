7e6c2069f6f3f0316288c874749b1c6c1b90cfef
Jakub Janšta <kubulambula@gmail.com>
2023-12-02 19:13:16 +0100

    built-in functions

--- src/built_in.h
+++ src/built_in.h
@@ -1,5 +1,5 @@
-#ifndef LEXER_H
-#define LEXER_H
+#ifndef BUILT_IN_H
+#define BUILT_IN_H
 
 
 /*
@@ -39,9 +39,24 @@
 // ----- WRITE FUNCTION -----
 
 #define FUNC_write \
-	"\n# Function write(...) (built-in)"\
+	"\n# Function write(_ argCnt: Int) (built-in)"\
 	"\nLABEL write"\
+	"\nLABEL writeLoop"\
+	"\nJUMPIFEQ writeLoopBreak LF@argCnt int@0"\
+	"\nSUB LF@argCnt LF@argCnt int@1"\
+	"\nCALL $write_helper"\
+	"\nJUMP writeLoop"\
+	"\nLABEL writeLoopBreak"\
 	"\nRETURN"
+
+#define FUNC_$write_helper \
+	"\n# Function $write_helper (built-in) - this function cannot be called by the user and is only inteded to be called by write()"\
+	"\nLABEL $write_helper"\
+	"\nCREATEFRAME"\
+	"\nDEFVAR TF@arg"\
+	"\nPOPS TF@arg"\
+	"\nWRITE TF@arg"\
+	"\nRETURN"\
 
 // ----- CONVERSION FUNCTIONS ------
 #define FUNC_Int2Double \
@@ -87,11 +102,40 @@
 	"\nGT LF@condition LF@j LF@len # Check if j > len"\
 	"\nJUMPIFEQ substring_return_nil LF@condition bool@true"\
 	"\n# Initial conditions passed. Now run the substring"\
-	"\n"\
+	"\nDEFVAR LF@substr # Variable with the substring to be returned"\
+	"\nDEFVAR LF@c # Helper variable with current char"\
+	"\nLABEL substringWhile"\
+	"\nJUMPIFEQ substringWhileBreak LF@i LF@j # if i == j goto substringWhileBreak"\
+	"\nGETCHAR LF@c LF@s LF@i # c = s[i]"\
+	"\nADD LF@i LF@i int@1 # i++"\
+	"\nCONCAT LF@substr LF@substr LF@c # substr = substr + c"\
+	"\nJUMP substringWhile"\
+	"\nLABEL substringWhileBreak"\
+	"\nPUSHS LF@substr"\
+	"\nRETURN"\
 	"\nLABEL substring_return_nil # Bad arguments for substring() - return nil"\
 	"\nPUSHS nil@nil"\
+	"\nRETURN"
+
+#define FUNC_ord \
+	"\n# Function ord(_ c: String)->Int"\
+	"\nLABEL ord"\
+	"\nDEFVAR LF@len"\
+	"\nSTRLEN LF@len LF@c"\
+	"\nJUMPIFQE ord_return_zero LF@len int@0"\
+	"\nDEFVAR LF@ordVal"\
+	"\nSTRI2INT LF@ordVal LF@c int@0"\
+	"\nPUSHS LF@ordVal"\
 	"\nRETURN"\
+	"\nLABEL ord_return_zero"\
+	"\nPUSHS int@0"\
+	"\nRETURN"
 
-
+#define FUNC_chr \
+	"\n# Function chr(_ i: Int)->String"\
+	"\nDEFVAR LF@chrVal"\
+	"\nINT2CHAR LF@chrVal LF@i"\
+	"\nPUSHS LF@chrVal"\
+	"\nRETURN"\
 
 #endif
