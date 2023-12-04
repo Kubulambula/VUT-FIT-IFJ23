bd01b54358c3778138825081044439065774e875
Petr Necas <petr.nec@seznam.cz>
2023-12-04 16:02:55 +0100

    ss

--- src/semantic.h
+++ src/semantic.h
@@ -11,5 +11,5 @@ Error appendScope(char**name,int scope);
 Error funcCallCheck(ASTNode*func,Type* returnType,SymTable* tables,SymTable* codeTable,int scope);
 
 Error handle_expression(exp_node* node, SymTable* tables, Type* returnType, SymTable *code_table, int scoping);
-Error handle_statements(ASTNode*statement,SymTable* tables,SymTable*codeTable,Type expected_type,int scoping,bool* returned);
+Error handle_statements(ASTNode*statement,SymTable* tables,SymTable*codeTable,Type expected_type,int scoping,bool* returned,bool nilable_return);
 #endif
